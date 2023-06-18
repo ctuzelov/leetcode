c61ad56e5809409af040cbd22d2386ba1f75b4e9
ctuzelov <ctuzelov@gmail.com>
2023-06-18 15:55:04 +0600

    test

--- main.go
+++ main.go
@@ -6,10 +6,16 @@ import (
 )
 
 func main() {
-	fmt.Println(backtracking.TotalNQueens(5))
-	fmt.Println(backtracking.TotalNQueens(6))
-	fmt.Println(backtracking.TotalNQueens(7))
-	fmt.Println(backtracking.TotalNQueens(8))
-	fmt.Println(backtracking.TotalNQueens(9))
 
+	board := [][]byte{
+		{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+		{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+		{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+		{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+		{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+		{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+		{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+		{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+		{'.', '.', '.', '.', '8', '.', '.', '7', '9'}}
+	fmt.Println(backtracking.SolveSudoku(board))
 }
