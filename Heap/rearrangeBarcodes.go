2c45f122dfd7126e91152490852eb5285c52c8f0
ctuzelov <110629909+ctuzelov@users.noreply.github.com>
2023-08-15 00:05:20 +0600

    1054. Distant Barcodes

---
+++ Heap/rearrangeBarcodes.go
@@ -0,0 +1,62 @@
+package Heap
+
+import "container/heap"
+
+type Counter struct {
+	num  int
+	freq int
+}
+
+type mxHeap []Counter
+
+func (h mxHeap) Len() int {
+	return len(h)
+}
+
+func (h mxHeap) Less(i, j int) bool {
+	return h[i].freq > h[j].freq
+}
+
+func (h mxHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *mxHeap) Push(x interface{}) {
+	(*h) = append((*h), x.(Counter))
+}
+
+func (h *mxHeap) Pop() interface{} {
+	toRemove := (*h)[len((*h))-1]
+	(*h) = (*h)[:len((*h))-1]
+	return toRemove
+}
+
+func RearrangeBarcodes(b []int) []int {
+	var res []int
+	m := make(map[int]int)
+	hp := &mxHeap{}
+	prevNum := 0
+	prevFreq := 0
+
+	for _, num := range b {
+		m[num]++
+	}
+
+	for k, v := range m {
+		heap.Push(hp, Counter{k, v})
+	}
+
+	for hp.Len() > 0 {
+		entry := heap.Pop(hp).(Counter)
+		
+		if prevFreq > 0{
+			heap.Push(hp, Counter{prevNum, prevFreq})
+		}
+
+		res = append(res, entry.num)
+		prevNum = entry.num
+		prevFreq = entry.freq
+		prevFreq--
+	}
+	return res
+}
